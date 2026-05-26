60d798a5d706e204b793fd8663103c07a66d177b
alkhwlyhasan777@gmail.com <alkhwlyhasan777@gmail.com>
2026-05-26 16:03:00 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import BtmHeader from "./components/header/BtmHeader";
 import TopHeader from "./components/header/TopHeader";
 import Home from "./pages/home/Home";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import ProductDetails from "./pages/product/ProductDetails";
 import CartItem from "./components/cart/CartItem";
 import HearttCart from "./components/heartCard/HearttCart";
@@ -19,16 +19,18 @@ function App() {
         <BtmHeader />
       </header>
 
-      <AnimatePresence mode="wait">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<CartItem />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/heartCart" element={<HearttCart />} />
-          <Route path="/category/:category" element={<CategoryPage/>} />
-        </Routes>
-      </AnimatePresence>
+      <BrowserRouter basename="/E-commerce">
+        <AnimatePresence mode="wait">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+            <Route path="/cart" element={<CartItem />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/heartCart" element={<HearttCart />} />
+            <Route path="/category/:category" element={<CategoryPage />} />
+          </Routes>
+        </AnimatePresence>
+      </BrowserRouter>
     </>
   );
 }
